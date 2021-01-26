dc3521cf3b3892492cd01a239173765342efccb1
Pierre <jpcampoverde9801@hotmail.com>
2021-01-25 20:04:11 -0500

    new commit

--- src/Components/Molecules/Program/Accordion.jsx
+++ src/Components/Molecules/Program/Accordion.jsx
@@ -8,8 +8,8 @@ const Accordion = (props) => {
   };
     return (
         <div className="w-full rounded-lg border-2 mb-2   transition-all ease-in duration-150">
-        <div className={`flex justify-between p-2  transition-all duration-200 ease-out  rounded-t-lg ${open?"bg-blue-100":""} `} onClick={handleClick}>
-          <p className="font-medium">{props.name}</p>
+        <div className={`flex justify-between px-2 py-3  transition-all duration-200 ease-out  rounded-t-lg ${open?"bg-blue-100":""} `} onClick={handleClick}>
+          <p className="font-medium text-lg">{props.name}</p>
           <RiArrowDownSLine className={`text-2xl my-auto transform transition-transform duration-300 ease-out ${open?"rotate-180":""}`} />
         </div>
         <article
